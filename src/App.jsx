e1ce0aea0a55abb5661012d0fd16c2495c69e83c
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-05-17 00:05:03 +0530

    Added necessary changes for deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import Navbar from "./components/Navbar.jsx"; // Importing the Navbar component
+import { Route, Routes } from "react-router-dom"; // Importing Route and Routes from React Router
+import HomePage from "./pages/HomePage"; // Importing the Home Page component
+import ProductDetailsPage from "./pages/ProductDetailsPage.jsx"; // Importing Product Details Page
+import CartPage from "./pages/CartPage.jsx"; // Importing Cart Page
+import { Toaster } from "react-hot-toast";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      <Toaster position="top-center" reverseOrder={false} />
+      {/* Navbar Component - Displayed at the top of all pages */}
+      <Navbar />
 
-export default App
+      {/* Main content container - Centers the Routes */}
+      <div className="flex items-center justify-center">
+        <Routes>
+          {/* Route for the home page */}
+          <Route path="/" element={<HomePage />} />
+
+          {/* Dynamic Route for product details - Uses ":id" to fetch specific product */}
+          <Route path="/product/:id" element={<ProductDetailsPage />} />
+
+          {/* Route for the shopping cart page */}
+          <Route path="/cart" element={<CartPage />} />
+        </Routes>
+      </div>
+    </>
+  );
+};
+
+export default App; // Exporting App component to be used in index.js
