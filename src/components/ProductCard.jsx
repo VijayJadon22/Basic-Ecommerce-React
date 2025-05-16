5fcb6ac5c21c687ac7716aaab6522daf65ad2f17
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-05-16 23:28:31 +0530

    Implemtned productCard for displaying products

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -4,12 +4,12 @@ const ProductCard = ({ product }) => {
   return (
     <div className="p-2 border border-gray-100 shadow-xl shadow-gray-400 rounded-lg flex flex-col justify-around items-center hover:scale-105 transition-all smooth duration-500">
       <img src={product.image} alt={product.title} width="100" />
-      <h3>{product.title}</h3>
-      <p>${product.price}</p>
-      <p>
+      <h3 className="font-semibold text-center mb-2">{product.title}</h3>
+      <p className="text-sm">${product.price}</p>
+      <p className="text-sm">
         ⭐ {product.rating.rate} ({product.rating.count} reviews)
       </p>
-      <Link to={`/product/${product.id}`}>View Details</Link>
+      <Link className="bg-gray-700 px-2 py-1 rounded-lg text-white mt-2" to={`/product/${product.id}`}>View Details</Link>
     </div>
   );
 };
